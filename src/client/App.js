026cec9c52461cc37cf681375bd15cdcc65c7c97
Christopher Smith <ctsmith2308@gmail.com>
2020-01-05 16:12:21 -0700

    add App.js to routes for persistent render

---
+++ src/client/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { renderRoutes } from "react-router-config";
+
+const App = ({ route }) => {
+  return (
+    <div>
+      <h1>Im the header</h1>
+      <div>{renderRoutes(route.routes)}</div>
+    </div>
+  );
+};
+
+export default {
+  component: App
+};
