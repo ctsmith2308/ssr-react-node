a68fb28975af44045141cc1d7d1172193f8f08b2
Christopher Smith <ctsmith2308@gmail.com>
2020-01-05 12:34:41 -0700

    serialize data from server to client

--- src/helpers/renderApp.js
+++ src/helpers/renderApp.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { renderToString } from "react-dom/server";
-import { StaticRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { StaticRouter } from "react-router-dom";
+import { renderToString } from "react-dom/server";
 import { renderRoutes } from "react-router-config";
+import serialize from "serialize-javascript";
 import Routes from "../client/Routes";
 
 export default (req, store) => {
@@ -20,7 +21,7 @@ export default (req, store) => {
       <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+          window.INITIAL_STATE = ${serialize(store.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>
