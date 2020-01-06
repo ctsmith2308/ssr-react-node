84b5310de16dbaba629202ad7d795cccd3c4fe87
Christopher Smith <ctsmith2308@gmail.com>
2020-01-05 17:07:16 -0700

    authentication added

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default () => (
-  <div>
-    <Link to="/"> React SSR</Link>
-  </div>
-);
+const Header = ({ auth }) => {
+  console.log("wat is my status", auth);
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
+  return (
+    <div>
+      <Link to="/"> React SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {authButton}
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+export default connect(mapStateToProps)(Header);
