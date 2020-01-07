d33bc43220eb80460313d3dbd6ffe26dcc08e6e7
Christopher Smith <ctsmith2308@gmail.com>
2020-01-06 19:27:43 -0700

    added react-helmet to html

--- src/helpers/renderApp.js
+++ src/helpers/renderApp.js
@@ -5,6 +5,7 @@ import { renderToString } from "react-dom/server";
 import { renderRoutes } from "react-router-config";
 import serialize from "serialize-javascript";
 import Routes from "../client/Routes";
+import { Helmet } from "react-helmet";
 
 export default (req, store, context) => {
   const content = renderToString(
@@ -14,10 +15,13 @@ export default (req, store, context) => {
       </StaticRouter>
     </Provider>
   );
+  const helmet = Helmet.renderStatic();
 
   return `
   <html>
     <head>
+    ${helmet.title.toString()}
+    ${helmet.meta.toString()}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     </head>
       <body>
